83d33b098bb9a97b02364eaeb73e39c5345b2813
Will Mitchell <wvmitchell@gmail.com>
2023-10-10 15:06:45 -0400

    added solution for triangle in typescript

---
+++ typescript/triangle/triangle.ts
@@ -0,0 +1,46 @@
+export class Triangle {
+  private sides: number[];
+
+  constructor(...sides: number[]) {
+    this.sides = sides;
+  }
+
+  get isEquilateral(): boolean {
+    const { sides, isTriangle } = this;
+    const allSidesEqual = sides.every((s) => s === sides[0]);
+
+    return isTriangle && allSidesEqual
+  }
+
+  get isIsosceles(): boolean {
+    const { sides, isTriangle } = this;
+    let twoSidesEqual = Boolean(sides.find((side, index) => {
+      const otherSides = sides.toSpliced(index, 1);
+      return otherSides.includes(side);
+    }))
+
+    return isTriangle && twoSidesEqual
+  }
+
+  get isScalene(): boolean {
+    const { sides, isTriangle } = this;
+    const allSidesInequal = sides.every((side, index) => {
+      const otherSides = sides.toSpliced(index, 1);
+      return !otherSides.includes(side);
+    });
+
+    return isTriangle && allSidesInequal
+  }
+
+  get isTriangle(): boolean {
+    const { sides } = this;
+    const [a, b, c] = sides;
+    const nonZeroLengths = sides.every((side) => side > 0);
+    const satisfiesInequality = sides.every((side, index) => {
+      const otherSides = sides.toSpliced(index, 1);
+      return otherSides.reduce((s, n) => s + n) >= side;
+    });
+
+    return nonZeroLengths && satisfiesInequality;
+  }
+}
