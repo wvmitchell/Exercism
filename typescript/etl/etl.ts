ace1083035b245d8d29de85eac673814f32ed6f8
Will Mitchell <wvmitchell@gmail.com>
2023-10-11 11:37:09 -0400

    make file pretty

--- typescript/etl/etl.ts
+++ typescript/etl/etl.ts
@@ -1,18 +1,18 @@
 type OldLetterValues = {
-  [key: string]: string[]
-}
+  [key: string]: string[];
+};
 
 type NewLetterValues = {
-  [key: string]: number
-}
+  [key: string]: number;
+};
 
 export function transform(old: OldLetterValues): NewLetterValues {
-  const entries = Object.entries(old)
+  const entries = Object.entries(old);
   return entries.reduce((newValues: NewLetterValues, info) => {
-    const [value, letters] = info
-    letters.forEach(letter => {
-      newValues[letter.toLowerCase()] = parseInt(value)
-    })
-    return newValues
-  }, {})
+    const [value, letters] = info;
+    letters.forEach((letter) => {
+      newValues[letter.toLowerCase()] = parseInt(value);
+    });
+    return newValues;
+  }, {});
 }
