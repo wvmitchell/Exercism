bfdd9cf2f068485a45d4be3f2a81a9a9b3eef079
Will Mitchell <wvmitchell@gmail.com>
2024-05-25 15:30:46 -0600

    working panagram solution

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,7 +1,5 @@
-=begin
-Write your code for the 'Pangram' exercise in this file. Make the tests in
-`pangram_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/pangram` directory.
-=end
+module Pangram
+  def self.pangram?(sentence)
+    sentence.downcase.scan(/[a-z]/).uniq.size == 26
+  end
+end
