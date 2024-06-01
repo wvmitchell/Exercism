d3cd9b2896ee60a0404dd46d4d297d6b53754005
Will Mitchell <wvmitchell@gmail.com>
2024-06-01 08:25:15 -0600

    added etl solution

--- typescript/etl/etl.ts
+++ typescript/etl/etl.ts
@@ -1,19 +1,14 @@
-type OldLetterValues = {
-  [key: string]: string[];
-};
+type OldLetterScores = { [key: string]: string[] }
+type LetterScores = { [key: string]: number }
 
-type NewLetterValues = {
-  [key: string]: number;
-};
+export function transform(old: OldLetterScores): LetterScores {
+  let newScores: LetterScores = {}
 
-export function transform(old: OldLetterValues): NewLetterValues {
-  const newValues: NewLetterValues = {}
-
-  for(const [value, letters] of Object.entries(old)) {
-    for(const letter of letters) {
-      newValues[letter.toLowerCase()] = parseInt(value)
-    }
+  for (let score in old) {
+    old[score].forEach(letter => {
+      newScores[letter.toLowerCase()] = parseInt(score)
+    })
   }
 
-  return newValues
+  return newScores
 }
