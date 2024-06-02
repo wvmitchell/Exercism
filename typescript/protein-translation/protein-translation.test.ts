e761d2f2da86fb871c176cdfbe08e0034d450972
Will Mitchell <wvmitchell@gmail.com>
2024-06-02 16:54:39 -0600

    protien translation wip

--- typescript/protein-translation/protein-translation.test.ts
+++ typescript/protein-translation/protein-translation.test.ts
@@ -31,120 +31,120 @@ describe('Translate input RNA sequences into proteins', () => {
     expect(translate('UCU')).toEqual(expected)
   })
 
-  it('Serine RNA sequence 2', () => {
+  xit('Serine RNA sequence 2', () => {
     const expected = ['Serine']
     expect(translate('UCC')).toEqual(expected)
   })
 
-  it('Serine RNA sequence 3', () => {
+  xit('Serine RNA sequence 3', () => {
     const expected = ['Serine']
     expect(translate('UCA')).toEqual(expected)
   })
 
-  it('Serine RNA sequence 4', () => {
+  xit('Serine RNA sequence 4', () => {
     const expected = ['Serine']
     expect(translate('UCG')).toEqual(expected)
   })
 
-  it('Tyrosine RNA sequence 1', () => {
+  xit('Tyrosine RNA sequence 1', () => {
     const expected = ['Tyrosine']
     expect(translate('UAU')).toEqual(expected)
   })
 
-  it('Tyrosine RNA sequence 2', () => {
+  xit('Tyrosine RNA sequence 2', () => {
     const expected = ['Tyrosine']
     expect(translate('UAC')).toEqual(expected)
   })
 
-  it('Cysteine RNA sequence 1', () => {
+  xit('Cysteine RNA sequence 1', () => {
     const expected = ['Cysteine']
     expect(translate('UGU')).toEqual(expected)
   })
 
-  it('Cysteine RNA sequence 2', () => {
+  xit('Cysteine RNA sequence 2', () => {
     const expected = ['Cysteine']
     expect(translate('UGC')).toEqual(expected)
   })
 
-  it('Tryptophan RNA sequence', () => {
+  xit('Tryptophan RNA sequence', () => {
     const expected = ['Tryptophan']
     expect(translate('UGG')).toEqual(expected)
   })
 
-  it('STOP codon RNA sequence 1', () => {
+  xit('STOP codon RNA sequence 1', () => {
     const expected: string[] = []
     expect(translate('UAA')).toEqual(expected)
   })
 
-  it('STOP codon RNA sequence 2', () => {
+  xit('STOP codon RNA sequence 2', () => {
     const expected: string[] = []
     expect(translate('UAG')).toEqual(expected)
   })
 
-  it('STOP codon RNA sequence 3', () => {
+  xit('STOP codon RNA sequence 3', () => {
     const expected: string[] = []
     expect(translate('UGA')).toEqual(expected)
   })
 
-  it('Sequence of two protein codons translates into proteins', () => {
+  xit('Sequence of two protein codons translates into proteins', () => {
     const expected = ['Phenylalanine', 'Phenylalanine']
     expect(translate('UUUUUU')).toEqual(expected)
   })
 
-  it('Sequence of two different protein codons translates into proteins', () => {
+  xit('Sequence of two different protein codons translates into proteins', () => {
     const expected = ['Leucine', 'Leucine']
     expect(translate('UUAUUG')).toEqual(expected)
   })
 
-  it('Translate RNA strand into correct protein list', () => {
+  xit('Translate RNA strand into correct protein list', () => {
     const expected = ['Methionine', 'Phenylalanine', 'Tryptophan']
     expect(translate('AUGUUUUGG')).toEqual(expected)
   })
 
-  it('Translation stops if STOP codon at beginning of sequence', () => {
+  xit('Translation stops if STOP codon at beginning of sequence', () => {
     const expected: string[] = []
     expect(translate('UAGUGG')).toEqual(expected)
   })
 
-  it('Translation stops if STOP codon at end of two-codon sequence', () => {
+  xit('Translation stops if STOP codon at end of two-codon sequence', () => {
     const expected = ['Tryptophan']
     expect(translate('UGGUAG')).toEqual(expected)
   })
 
-  it('Translation stops if STOP codon at end of three-codon sequence', () => {
+  xit('Translation stops if STOP codon at end of three-codon sequence', () => {
     const expected = ['Methionine', 'Phenylalanine']
     expect(translate('AUGUUUUAA')).toEqual(expected)
   })
 
-  it('Translation stops if STOP codon in middle of three-codon sequence', () => {
+  xit('Translation stops if STOP codon in middle of three-codon sequence', () => {
     const expected = ['Tryptophan']
     expect(translate('UGGUAGUGG')).toEqual(expected)
   })
 
-  it('Translation stops if STOP codon in middle of six-codon sequence', () => {
+  xit('Translation stops if STOP codon in middle of six-codon sequence', () => {
     const expected = ['Tryptophan', 'Cysteine', 'Tyrosine']
     expect(translate('UGGUGUUAUUAAUGGUUU')).toEqual(expected)
   })
 
-  it("Non-existing codon can't translate", () => {
+  xit("Non-existing codon can't translate", () => {
     expect(() => {
       translate('AAA')
     }).toThrowError('Invalid codon')
   })
 
-  it("Unknown amino acids, not part of a codon, can't translate", () => {
+  xit("Unknown amino acids, not part of a codon, can't translate", () => {
     expect(() => {
       translate('XYZ')
     }).toThrowError('Invalid codon')
   })
 
-  it("Incomplete RNA sequence can't translate", () => {
+  xit("Incomplete RNA sequence can't translate", () => {
     expect(() => {
       translate('AUGU')
     }).toThrowError('Invalid codon')
   })
 
-  it('Incomplete RNA sequence can translate if valid until a STOP codon', () => {
+  xit('Incomplete RNA sequence can translate if valid until a STOP codon', () => {
     const expected = ['Phenylalanine', 'Phenylalanine']
     expect(translate('UUCUUCUAAUGGU')).toEqual(expected)
   })
--- typescript/protein-translation/protein-translation.ts
+++ typescript/protein-translation/protein-translation.ts
@@ -1,48 +1,12 @@
-type Protein = 
-  'Methionine' | 
-  'Phenylalanine' | 
-  'Leucine' | 
-  'Serine' |
-  'Tyrosine' |
-  'Cysteine' |
-  'Tryptophan' |
-  'STOP' |
-  'ERROR'
-
-const CODONS = [
-  'AUG', 'UUA', 'UUG', 'UUC', 'UUU', 'UCA', 'UCG', 'UCU', 'UCC', 
-  'UAC', 'UAU', 'UGU', 'UGC', 'UGG', 'UAA', 'UAG', 'UGA']
-
-export function translate(rna: string): Protein[] {
-  const codons = rna.match(/[A-Z]{3}|[A-Z]+$/g) || []
-  const proteins = codons.map(codon => parseCodon(codon))
-  const stop = proteins.indexOf('STOP')
-
-  if(stop >= 0) proteins.splice(stop)
-  if(proteins.includes('ERROR')) throw Error('Invalid codon')
-
-  return proteins
+const codonToProtein: { [key: string]: string } = {
+  AUG: 'Methionine',
+  UUU: 'Phenylalanine',
+  UUC: 'Phenylalanine',
+  UUA: 'Leucine',
+  UUG: 'Leucine',
+  UCU: 'Serine',
 }
 
-function parseCodon(codon: string): Protein {
-  switch(codon) {
-    case 'AUG':
-      return 'Methionine'
-    case 'UUA': case 'UUG':
-      return 'Leucine'
-    case 'UUC': case 'UUU':
-      return 'Phenylalanine'
-    case 'UCA': case 'UCG': case 'UCU': case 'UCC':
-      return 'Serine'
-    case 'UAC': case 'UAU':
-      return 'Tyrosine'
-    case 'UGU': case 'UGC':
-      return 'Cysteine'
-    case 'UGG':
-      return 'Tryptophan'
-    case 'UAA': case 'UAG': case 'UGA':
-      return 'STOP'
-    default:
-      return 'ERROR'
-  }
+export function translate(rna: string): string[] {
+  return rna.match(/\w{3}/g)?.map(m => codonToProtein[m]) || []
 }
