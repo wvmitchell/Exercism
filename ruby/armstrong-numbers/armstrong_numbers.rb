9270d7773f5613f495ed8387986afc42bf729740
Will Mitchell <wvmitchell@gmail.com>
2024-06-03 09:05:00 -0600

    improve solution to make use of integer properties

--- ruby/armstrong-numbers/armstrong_numbers.rb
+++ ruby/armstrong-numbers/armstrong_numbers.rb
@@ -1,7 +1,7 @@
 module ArmstrongNumbers
   def self.include?(number)
-    chars = number.to_s.chars
-    length = chars.length
-    number == chars.reduce(0) { |sum, char| sum + char.to_i**length }
+    digits = number.digits
+    length = digits.length
+    number == digits.sum { |digit| digit**length }
   end
 end
