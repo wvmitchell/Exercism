6e390725a354d09c00e462e4ed905e87aea8e520
Will Mitchell <wvmitchell@gmail.com>
2024-06-03 08:46:49 -0600

    alternative implementation

--- typescript/raindrops/raindrops.ts
+++ typescript/raindrops/raindrops.ts
@@ -1,7 +1,12 @@
+const dropNames: { [key: number]: string } = {
+  3: 'Pling',
+  5: 'Plang',
+  7: 'Plong',
+}
+
 export function convert(drops: number): string {
   let statement = ''
-  if(drops % 3 === 0) statement += 'Pling'
-  if(drops % 5 === 0) statement += 'Plang'
-  if(drops % 7 === 0) statement += 'Plong'
+  for(let d in dropNames)
+    if(drops % parseInt(d) === 0) statement += dropNames[d]
   return statement.length > 0 ? statement : String(drops)
 }
