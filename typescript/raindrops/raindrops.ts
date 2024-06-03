7b7fadfadc8d334c58c0d0ba6b0ac91794b35294
Will Mitchell <wvmitchell@gmail.com>
2024-06-03 08:28:33 -0600

    working solution for raindrops

--- typescript/raindrops/raindrops.ts
+++ typescript/raindrops/raindrops.ts
@@ -1,16 +1,7 @@
-const dropWords: [number, string][] = [
-  [3, "Pling"],
-  [5, "Plang"],
-  [7, "Plong"],
-]
-
 export function convert(drops: number): string {
-  let statement = ""
-  //drops % 3 === 0 && (statement += "Pling")
-  //drops % 5 === 0 && (statement += "Plang")
-  //drops % 7 === 0 && (statement += "Plong")
-  for (let [num, word] of dropWords) {
-    if(drops % num === 0) statement += word
-  }
-  return statement || `${drops}`
+  let statement = ''
+  if(drops % 3 === 0) statement += 'Pling'
+  if(drops % 5 === 0) statement += 'Plang'
+  if(drops % 7 === 0) statement += 'Plong'
+  return statement.length > 0 ? statement : String(drops)
 }
