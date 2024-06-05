7c1c949c95f0b8f2ef9b34d83e5b35c10f4cb211
Will Mitchell <wvmitchell@gmail.com>
2024-06-05 10:16:48 -0600

    working solution for resistor color trio

--- ruby/resistor-color-trio/resistor_color_trio.rb
+++ ruby/resistor-color-trio/resistor_color_trio.rb
@@ -1,41 +1,40 @@
-# typed: strict
-
-RESISTOR_CODES = T.let({
-  black: 0,
-  brown: 1,
-  red: 2,
-  orange: 3,
-  yellow: 4,
-  green: 5,
-  blue: 6,
-  violet: 7,
-  grey: 8,
-  white: 9
-}.freeze, T::Hash[T.untyped, T.untyped])
+COLOR_CODES = {
+  "black" => 0,
+  "brown" => 1,
+  "red" => 2,
+  "orange" => 3,
+  "yellow" => 4,
+  "green" => 5,
+  "blue" => 6,
+  "violet" => 7,
+  "grey" => 8,
+  "white" => 9
+}.freeze
 
 class ResistorColorTrio
-  extend T::Sig
-
-  sig { params(colors: T::Array[String]).void }
   def initialize(colors)
-    @ohms = T.let(calculate_ohms(colors), Integer)
+    @colors = colors
+    @ohms = calculate_ohms
   end
 
-  sig { returns(String) }
   def label
-    if @ohms >= 1000
-      "Resistor value: #{@ohms / 1000} kiloohms"
-    else
-      "Resistor value: #{@ohms} ohms"
-    end
+    "Resistor value: #{format_ohms}"
   end
 
   private
 
-  def calculate_ohms(colors)
-    tens = RESISTOR_CODES[T.must(colors[1]).to_sym] * 10
-    units = RESISTOR_CODES[T.must(colors[1]).to_sym]
-    factor = 10**RESISTOR_CODES[T.must(colors[2]).to_sym]
-    (tens + units) * factor
+  def calculate_ohms
+    tens = COLOR_CODES[@colors[0]] * 10
+    units = COLOR_CODES[@colors[1]]
+    multiplier = 10 ** COLOR_CODES[@colors[2]]
+    (tens + units) * multiplier
+  end
+
+  def format_ohms
+    if @ohms < 1000
+      "#{@ohms} ohms"
+    else
+      "#{@ohms / 1000} kiloohms"
+    end
   end
 end
