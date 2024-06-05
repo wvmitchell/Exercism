0e0fa395833d05539dabb9cdd17dfc31529bbdcd
Will Mitchell <wvmitchell@gmail.com>
2024-06-05 08:06:04 -0600

    working solution

--- typescript/nucleotide-count/nucleotide-count.ts
+++ typescript/nucleotide-count/nucleotide-count.ts
@@ -1,15 +1,18 @@
-type Nucleotide = 'A' | 'C' | 'G' | 'T'
-type NucleotideCounts = {
-  [key in Nucleotide]: number
-}
-
-export function nucleotideCounts(strand: string): NucleotideCounts {
-  if(strand.search(/[^ACGT]/g) !== -1) {
-    throw Error('Invalid nucleotide in strand')
+export function nucleotideCounts(strand: string): { [key: string]: number } {
+  if(strand.match(/[^ACGT]/)) {
+    throw new Error('Invalid nucleotide in strand')
   }
 
-  return [...strand].reduce((counts, n) => {
-    counts[n as Nucleotide] += 1
-    return counts
-  }, { A: 0, C: 0, G: 0, T: 0 })
+  let counts: { [key: string]: number } = {
+    A: 0,
+    C: 0,
+    G: 0,
+    T: 0
+  }
+
+  for(let nucleotide of strand) {
+    counts[nucleotide] += 1
+  }
+
+  return counts
 }
