2895b392884098e0d55eb4b3222a445705e43c0c
Will Mitchell <wvmitchell@gmail.com>
2024-06-05 10:32:30 -0600

    get rid of secondary initializer

--- ruby/nucleotide-count/nucleotide_count.rb
+++ ruby/nucleotide-count/nucleotide_count.rb
@@ -1,19 +1,17 @@
 class Nucleotide
-  attr_reader :histogram
+  attr_accessor :histogram
 
-  def initialize(dna)
+  def self.from_dna(dna)
     raise ArgumentError if dna.match(/[^ATCG]/)
 
-    @histogram = {
+    nuc = Nucleotide.new
+    nuc.histogram = {
       'A' => dna.scan(/A/).count,
       'T' => dna.scan(/T/).count,
       'C' => dna.scan(/C/).count,
       'G' => dna.scan(/G/).count
     }
-  end
-
-  def self.from_dna(dna)
-    new(dna)
+    nuc
   end
 
   def count(nucleotide)
