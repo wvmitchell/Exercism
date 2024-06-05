effd3d9a6bdca9f02069dd08ff831e9e02a6a947
Will Mitchell <wvmitchell@gmail.com>
2024-06-05 12:05:24 -0600

    working solution

--- typescript/scrabble-score/scrabble-score.ts
+++ typescript/scrabble-score/scrabble-score.ts
@@ -1,14 +1,36 @@
-type LetterValues = {
-  [key: string]: number
+const scores: { [key: string]: number } = {
+  'a': 1,
+  'b': 3,
+  'c': 3,
+  'd': 2,
+  'e': 1,
+  'f': 4,
+  'g': 2,
+  'h': 4,
+  'i': 1,
+  'j': 8,
+  'k': 5,
+  'l': 1,
+  'm': 3,
+  'n': 1,
+  'o': 1,
+  'p': 3,
+  'q': 10,
+  'r': 1,
+  's': 1,
+  't': 1,
+  'u': 1,
+  'v': 4,
+  'w': 4,
+  'x': 8,
+  'y': 4,
+  'z': 10,
 }
 
-const LETTER_VALUES: LetterValues = {
-  A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1,
-  J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10, R: 1,
-  S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z: 10
-}
-
-export function score(word: string = ''): number {
-  const chars = word.toUpperCase().split('')
-  return chars.reduce((sum, char) => sum + LETTER_VALUES[char], 0)
+export function score(word?: string): number {
+  if (!word) return 0
+  return word
+    .toLowerCase()
+    .split('')
+    .reduce((sum, letter) => sum + scores[letter], 0)
 }
