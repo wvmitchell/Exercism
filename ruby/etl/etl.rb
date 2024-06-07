bad01f679aec6a0328500fe1f7104c66ac7674c3
Will Mitchell <wvmitchell@gmail.com>
2024-06-07 08:41:38 -0600

    alternative solution using to_h and merge

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -1,7 +1,7 @@
 module ETL
   def self.transform(counts)
     counts.each_with_object({}) do |(count, letters), transformed|
-      letters.each { |letter| transformed[letter.downcase] = count }
+      transformed.merge!(letters.to_h { |letter| [letter.downcase, count] })
     end
   end
 end
