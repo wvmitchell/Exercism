1293b3d7c5fde8f3dfd4c0b26739e3ba830b794d
Will Mitchell <wvmitchell@gmail.com>
2024-06-09 08:21:34 -0600

    refactored solution

--- typescript/luhn/luhn.ts
+++ typescript/luhn/luhn.ts
@@ -2,12 +2,19 @@ export function valid(digitString: string): boolean {
   if(digitString.match(/[^\d|\s]/g)) return false
   if(digitString.replace(/\s/g, '').length <= 1) return false
 
-  var digits = digitString.match(/\d/g)?.map(val => parseInt(val))
-  var modified = digits?.reverse().map((digit, index) => {
-    let tmp = index % 2 === 1 ? digit * 2 : digit
-    return tmp > 9 ? tmp - 9 : tmp
-  })
-  var total = modified?.reduce((sum, num) => sum + num)
+  var digitStrings = digitString.match(/\d/g)?.reverse()
+  var total = digitStrings?.map(Number)
+                           .map(toLuhn)
+                           .reduce(sum)
 
   return Number(total) % 10 === 0
+}
+
+function toLuhn(digit: number, index: number): number {
+    let tmp = index % 2 === 1 ? digit * 2 : digit
+    return tmp > 9 ? tmp - 9 : tmp
+}
+
+function sum(num1: number, num2: number): number {
+  return num1 + num2
 }
