9d14b4f7b5b413ffe103b5a29bc701593b58186c
Will Mitchell <wvmitchell@gmail.com>
2024-06-10 22:34:44 -0600

    updated method to use each_cons rather than a range and indexes

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -1,13 +1,22 @@
 class Proverb
-  def initialize(*chain, qualifier: nil)
+  def initialize(*chain, qualifier: '')
     @chain = chain
-    @qualifier = qualifier ? "#{qualifier} " : ''
+    @qualifier = qualifier
   end
 
   def to_s
-    @chain[0..-2].each_with_object('').with_index do |(word, str), i|
-      next_word = @chain[i + 1]
+    main + final
+  end
+
+  private
+
+  def main
+    @chain.each_cons(2).each_with_object('') do |(word, next_word), str|
       str << "For want of a #{word} the #{next_word} was lost.\n"
-    end + "And all for the want of a #{@qualifier + @chain[0]}."
+    end
+  end
+
+  def final
+    "And all for the want of a #{@qualifier} #{@chain.first}.".squeeze(' ')
   end
 end
