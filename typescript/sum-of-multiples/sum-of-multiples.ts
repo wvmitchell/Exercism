6c22c4524d2ce20481c3d3af6a4070fcffa09164
Will Mitchell <wvmitchell@gmail.com>
2024-06-15 19:56:41 -0600

    make solution more semantic

--- typescript/sum-of-multiples/sum-of-multiples.ts
+++ typescript/sum-of-multiples/sum-of-multiples.ts
@@ -2,10 +2,8 @@ export function sum(factors: number[], limit: number): number {
   let multiples: number[] = []
 
   for(let i = 0; i < limit; i++) {
-    if(factors.some(n => i % n === 0)) {
-      multiples.push(i)
-    }
+    if(factors.some(factor => i % factor === 0)) multiples.push(i)
   }
 
-  return multiples.reduce((acc, num) => acc + num, 0)
+  return multiples.reduce((total, num) => total + num, 0)
 }
