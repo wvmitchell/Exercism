c8f889f6851ce146a0fdd61c163a48ab10188b54
Will Mitchell <wvmitchell@gmail.com>
2024-06-19 10:10:45 -0600

    remove unnecessary call to floor

--- ruby/binary-search/binary_search.rb
+++ ruby/binary-search/binary_search.rb
@@ -3,7 +3,7 @@ class BinarySearch
     @array = array
     @left = 0
     @right = array.length - 1
-    @mid = (array.length / 2).floor
+    @mid = array.length / 2
   end
 
   def search_for(target)
@@ -15,7 +15,7 @@ class BinarySearch
       else
         @left = @mid + 1
       end
-      @mid = ((@left + @right) / 2).floor
+      @mid = (@left + @right) / 2
     end
   end
 end
