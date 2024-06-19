120ca44c7ac6d773d3004d40cb315b64b4b128f9
Will Mitchell <wvmitchell@gmail.com>
2024-06-19 11:26:12 -0600

    simpler solution

--- typescript/anagram/anagram.ts
+++ typescript/anagram/anagram.ts
@@ -1,5 +1,3 @@
-type LetterCounts = { [key: string]: number };
-
 export class Anagram {
   test: string;
 
@@ -10,31 +8,10 @@ export class Anagram {
   public matches(...potentials: string[]): string[] {
     return potentials.filter((potential) => {
       potential = potential.toLowerCase();
+      let sortedTest = this.test.split("").sort().join("");
+      let sortedPotential = potential.split("").sort().join("");
 
-      return (
-        this.test !== potential &&
-        this.test.length === potential.length &&
-        this.lettersMatch(this.test, potential)
-      );
-    });
-  }
-
-  private countLetters(input: string): LetterCounts {
-    let counts: { [key: string]: number } = {};
-    for (let char of input) {
-      let count: number | undefined = counts[char];
-      count ? counts[char]++ : (counts[char] = 1);
-    }
-
-    return counts;
-  }
-
-  private lettersMatch(str1: string, str2: string): boolean {
-    let counts1 = this.countLetters(str1);
-    let counts2 = this.countLetters(str2);
-
-    return Object.keys(counts1).every((key) => {
-      return counts1[key] === counts2[key];
+      return this.test !== potential && sortedTest === sortedPotential;
     });
   }
 }
