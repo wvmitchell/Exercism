8805d9ae077a8ee63ff89cfc0879a8ef3f961ff1
Will Mitchell <wvmitchell@gmail.com>
2024-06-19 10:04:36 -0600

    solution for binary search in ruby

---
+++ ruby/binary-search/binary_search.rb
@@ -0,0 +1,21 @@
+class BinarySearch
+  def initialize(array)
+    @array = array
+    @left = 0
+    @right = array.length - 1
+    @mid = (array.length / 2).floor
+  end
+
+  def search_for(target)
+    while @left <= @right
+      return @mid if target == @array[@mid]
+
+      if target < @array[@mid]
+        @right = @mid - 1
+      else
+        @left = @mid + 1
+      end
+      @mid = ((@left + @right) / 2).floor
+    end
+  end
+end
