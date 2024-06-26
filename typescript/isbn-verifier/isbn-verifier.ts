3ef9c68b2333fabe300f5ab091975c87fa933ec0
Will Mitchell <wvmitchell@gmail.com>
2024-06-25 23:17:37 -0500

    working solution for isbn verifier in ts

---
+++ typescript/isbn-verifier/isbn-verifier.ts
@@ -0,0 +1,35 @@
+const charValues: { [key: string]: number } = {
+  0: 0,
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  X: 10,
+};
+
+export function isValid(isbn: string): boolean {
+  let chars = isbn.match(/\d|X/g);
+
+  if (!isbn.match(/^[0-9|-]*[0-8|X]$/)) {
+    return false;
+  } else if (chars?.length !== 10) {
+    return false;
+  } else if (chars) {
+    let total = 0;
+
+    for (let i = 10; i >= 1; i--) {
+      let char = chars[10 - i];
+      let value = charValues[char];
+      total += i * value;
+    }
+
+    return total % 11 === 0;
+  }
+
+  return false;
+}
