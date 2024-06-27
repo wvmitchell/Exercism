be77b170760678bb9469d934d20b8c885b948884
Will Mitchell <wvmitchell@gmail.com>
2024-06-27 10:20:40 -0500

    passing solution for card tricks in go

---
+++ go/card-tricks/card_tricks.go
@@ -0,0 +1,47 @@
+// Package cards provides functions for working with slices of integers.
+package cards
+
+// FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
+func FavoriteCards() []int {
+	return []int{2, 6, 9}
+}
+
+// GetItem retrieves an item from a slice at given position.
+// If the index is out of range, we want it to return -1.
+func GetItem(slice []int, index int) int {
+	if indexInRange(slice, index) {
+		return slice[index]
+	}
+	return -1
+}
+
+func indexInRange(slice []int, index int) bool {
+	return index >= 0 && index < len(slice)
+}
+
+// SetItem writes an item to a slice at given position overwriting an existing value.
+// If the index is out of range the value needs to be appended.
+func SetItem(slice []int, index, value int) []int {
+	if indexInRange(slice, index) {
+		slice[index] = value
+		return slice
+	}
+	return append(slice, value)
+}
+
+// PrependItems adds an arbitrary number of values at the front of a slice.
+func PrependItems(slice []int, values ...int) []int {
+	prependSlice := []int{}
+	prependSlice = append(prependSlice, values...)
+	return append(prependSlice, slice...)
+}
+
+// RemoveItem removes an item from a slice by modifying the existing slice.
+func RemoveItem(slice []int, index int) []int {
+	if indexInRange(slice, index) {
+		left := slice[:index]
+		right := slice[(index + 1):]
+		return append(left, right...)
+	}
+	return slice
+}
