386136ff8aa2e9a09abbac47912b455b87e6de33
Will Mitchell <wvmitchell@gmail.com>
2024-06-29 14:41:24 -0500

    solution for nfs in go

---
+++ go/need-for-speed/need_for_speed.go
@@ -0,0 +1,47 @@
+// Package speed implements a remote controlled car with a battery that can be driven on a track.
+package speed
+
+// Car represents a remote controlled car.
+type Car struct {
+	speed        int
+	batteryDrain int
+	battery      int
+	distance     int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{
+		speed:        speed,
+		batteryDrain: batteryDrain,
+		battery:      100,
+		distance:     0,
+	}
+}
+
+// Track represents a track that the car can drive on.
+type Track struct {
+	distance int
+}
+
+// NewTrack creates a new track
+func NewTrack(distance int) Track {
+	return Track{
+		distance: distance,
+	}
+}
+
+// Drive drives the car one time. If there is not enough battery to drive one more time,
+// the car will not move.
+func Drive(car Car) Car {
+	if car.battery >= car.batteryDrain {
+		car.distance += car.speed
+		car.battery -= car.batteryDrain
+	}
+	return car
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	return (car.battery / car.batteryDrain * car.speed) >= track.distance
+}
