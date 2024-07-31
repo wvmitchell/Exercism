7a8132003bf9441c4ce207e5b4f2f52de1efdd03
Will Mitchell <wvmitchell@gmail.com>
2024-07-31 10:07:56 -0400

    passing solution for sorting room in go

---
+++ go/sorting-room/sorting_room.go
@@ -0,0 +1,76 @@
+// Package sorting provides the sorting functions for the factory
+package sorting
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func formatFloat(f float64) string {
+	return strconv.FormatFloat(f, 'f', 1, 64)
+}
+
+// DescribeNumber should return a string describing the number.
+func DescribeNumber(f float64) string {
+	return fmt.Sprintf("This is the number %s", formatFloat(f))
+}
+
+// NumberBox is the interface for numbers
+type NumberBox interface {
+	Number() int
+}
+
+// DescribeNumberBox should return a string describing the NumberBox.
+func DescribeNumberBox(nb NumberBox) string {
+	float := float64(nb.Number())
+	return fmt.Sprintf("This is a box containing the number %s", formatFloat(float))
+}
+
+// FancyNumber holds onto a fancy number
+type FancyNumber struct {
+	n string
+}
+
+func (i FancyNumber) Value() string {
+	return i.n
+}
+
+type FancyNumberBox interface {
+	Value() string
+}
+
+// ExtractFancyNumber should return the integer value for a FancyNumber
+// and 0 if any other FancyNumberBox is supplied.
+func ExtractFancyNumber(fnb FancyNumberBox) int {
+	fancyNumber, ok := fnb.(FancyNumber)
+	if !ok {
+		return 0
+	}
+	val, err := strconv.Atoi(fancyNumber.Value())
+	if err != nil {
+		return 0
+	}
+	return val
+}
+
+// DescribeFancyNumberBox should return a string describing the FancyNumberBox.
+func DescribeFancyNumberBox(fnb FancyNumberBox) string {
+	float := float64(ExtractFancyNumber(fnb))
+	return fmt.Sprintf("This is a fancy box containing the number %s", formatFloat(float))
+}
+
+// DescribeAnything should return a string describing whatever it contains.
+func DescribeAnything(i interface{}) string {
+	switch v := i.(type) {
+	case int:
+		return DescribeNumber(float64(v))
+	case float64:
+		return DescribeNumber(v)
+	case NumberBox:
+		return DescribeNumberBox(v)
+	case FancyNumberBox:
+		return DescribeFancyNumberBox(v)
+	default:
+		return "Return to sender"
+	}
+}
