8c64ad48d0a4867b21b079540ad9e43ff3fddbe7
Will Mitchell <wvmitchell@gmail.com>
2024-08-18 06:35:55 -0400

    convert to boolean chain

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -3,10 +3,5 @@ package leap
 
 // IsLeapYear determines if a year is a leap year.
 func IsLeapYear(year int) bool {
-	if year%4 == 0 && year%100 != 0 {
-		return true
-	} else if year%400 == 0 {
-		return true
-	}
-	return false
+	return year%4 == 0 && (year%400 == 0 || year%100 != 0)
 }
