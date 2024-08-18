00f20fd9ab25a466bc4231ebd2422a281d973f62
Will Mitchell <wvmitchell@gmail.com>
2024-08-18 06:36:34 -0400

    solution to scrabble score in go

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -3,39 +3,39 @@ package scrabble
 
 import "strings"
 
+var values = map[rune]int{
+	'a': 1,
+	'b': 3,
+	'c': 3,
+	'd': 2,
+	'e': 1,
+	'f': 4,
+	'g': 2,
+	'h': 4,
+	'i': 1,
+	'j': 8,
+	'k': 5,
+	'l': 1,
+	'm': 3,
+	'n': 1,
+	'o': 1,
+	'p': 3,
+	'q': 10,
+	'r': 1,
+	's': 1,
+	't': 1,
+	'u': 1,
+	'v': 4,
+	'w': 4,
+	'x': 8,
+	'y': 4,
+	'z': 10,
+}
+
 // Score computes the Scrabble score for a word.
 func Score(word string) (score int) {
-	values := map[string]int{
-		"A": 1,
-		"B": 3,
-		"C": 3,
-		"D": 2,
-		"E": 1,
-		"F": 4,
-		"G": 2,
-		"H": 4,
-		"I": 1,
-		"J": 8,
-		"K": 5,
-		"L": 1,
-		"M": 3,
-		"N": 1,
-		"O": 1,
-		"P": 3,
-		"Q": 10,
-		"R": 1,
-		"S": 1,
-		"T": 1,
-		"U": 1,
-		"V": 4,
-		"W": 4,
-		"X": 8,
-		"Y": 4,
-		"Z": 10,
-	}
-
-	for _, letter := range word {
-		score += values[strings.ToUpper(string(letter))]
+	for _, rune := range strings.ToLower(word) {
+		score += values[rune]
 	}
 	return score
 }
