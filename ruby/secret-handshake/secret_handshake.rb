e522d671206598dc96488d32eab84ff9a3ef9091
Will Mitchell <wvmitchell@gmail.com>
2024-08-21 10:37:21 -0400

    solution to secret handshake in ruby

---
+++ ruby/secret-handshake/secret_handshake.rb
@@ -0,0 +1,38 @@
+class SecretHandshake
+  # @!attribute [r] commands
+  #   @return [Array<String>] the commands that make up the handshake
+  attr_reader :commands
+
+  # The possible commands in a secret_handshake
+  # @return [Hash<Integer, String>]
+  POSSIBLE_COMMANDS = {
+    1 => "wink",
+    2 => "double blink",
+    4 => "close your eyes",
+    8 => "jump"
+  }.freeze
+
+  # The code causing the handshake to be reversed
+  # @return [Integer]
+  REVERSE_COMMAND = 16
+
+  # initialize a new SecretHandshake
+  # @param code [Integer] the code to be used to generate the handshake
+  # @return [NilClass]
+  def initialize(code)
+    @commands = parse_code(code)
+  end
+
+  # parse_code takes an integer code and does bitwise comparisons
+  # to determine the corrrect commands
+  # @param code [Integer] the code to be used to generate the handshake
+  # @return [Array<String>] the commands that make up the handshake
+  def parse_code(code)
+    code = code.is_a?(Integer) ? code : 0
+    list =
+      POSSIBLE_COMMANDS
+      .map { |int, command| (code & int).positive? ? command : nil }
+      .compact
+    (code & REVERSE_COMMAND).positive? ? list.reverse : list
+  end
+end
