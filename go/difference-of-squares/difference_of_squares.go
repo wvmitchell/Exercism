3a6e355e6a10ad980caa489a7962e9d719ab6ab6
Will Mitchell <wvmitchell@gmail.com>
2024-08-21 13:22:45 -0400

    solution to diff of squares in go

---
+++ go/difference-of-squares/difference_of_squares.go
@@ -0,0 +1,20 @@
+// Package diffsquares provides functions to calculate the difference between the square of the sum and the sum of the squares of the first N natural numbers.
+package diffsquares
+
+import "math"
+
+// SquareOfSum returns the square of the sum of the first N natural numbers.
+func SquareOfSum(n int) int {
+	sum := (n * (1 + n)) / 2
+	return int(math.Pow(float64(sum), float64(2)))
+}
+
+// SumOfSquares returns the sum of the squares of the first N natural numbers.
+func SumOfSquares(n int) int {
+	return int((n * (n + 1) * (2*n + 1)) / 6)
+}
+
+// Difference returns the difference between the square of the sum and the sum of the squares of the first N natural numbers.
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
