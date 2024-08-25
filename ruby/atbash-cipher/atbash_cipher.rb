d1c504ec03756856526ab03a9caa9055c56f0088
Will Mitchell <wvmitchell@gmail.com>
2024-08-25 10:17:51 -0400

    refactor atbash

--- ruby/atbash-cipher/atbash_cipher.rb
+++ ruby/atbash-cipher/atbash_cipher.rb
@@ -1,28 +1,14 @@
 module Atbash
   # LETTERS is a constant that holds the alphabet
   # @return [String] the alphabet
-  LETTERS = ("a".."z").to_a
-
-  # KEY is a constant hash that holds the alphabet and its reverse
-  # @return [Hash<String, String>] the alphabet and its reverse
-  KEY = LETTERS.zip(LETTERS.reverse).to_h
+  LETTERS = ("a".."z").to_a.join
 
   # encode is a class method that takes a plaintext string
   # and returns a ciphertext string
   # @param plaintext [String] the plaintext String
   # @return [String] the ciphertext String
   def self.encode(plaintext)
-    chars = plaintext.downcase.chars
-    chars
-      .map do |char|
-        next if char.match?(/\W/)
-
-        KEY[char] || char
-      end
-      .compact
-      .each_slice(5)
-      .map(&:join)
-      .join(" ")
+    decode(plaintext).chars.each_slice(5).map(&:join).join(" ")
   end
 
   # decode is a class method that takes a ciphertext String
@@ -30,6 +16,6 @@ module Atbash
   # @param ciphertext [String] the ciphertext String
   # @return [String] the plaintext String
   def self.decode(ciphertext)
-    ciphertext.delete(" ").chars.map { |char| KEY[char] || char }.join
+    ciphertext.downcase.gsub(/\W/, "").tr(LETTERS, LETTERS.reverse)
   end
 end
