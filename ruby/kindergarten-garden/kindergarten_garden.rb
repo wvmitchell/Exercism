e09cf172fa8c37560acf03f30b7a571d5ba163ac
Will Mitchell <wvmitchell@gmail.com>
2024-08-28 12:49:56 -0400

    solution to kindergarten garden in ruby

---
+++ ruby/kindergarten-garden/kindergarten_garden.rb
@@ -0,0 +1,49 @@
+class Garden
+  CHILDREN = %i[
+    alice
+    bob
+    charlie
+    david
+    eve
+    fred
+    ginny
+    harriet
+    ileana
+    joseph
+    kincaid
+    larry
+  ].freeze
+  PLANTS = {
+    "C" => :clover,
+    "G" => :grass,
+    "R" => :radishes,
+    "V" => :violets
+  }.freeze
+
+  def initialize(garden_rows)
+    @plants = parse_rows(garden_rows)
+  end
+
+  def method_missing(method, *args)
+    super unless CHILDREN.include?(method)
+
+    self
+      .class
+      .send(:define_method, method) do
+        index = CHILDREN.index(method) * 2
+        @plants.map { |row| row.slice(index, 2) }.flatten
+      end
+
+    send(method)
+  end
+
+  def respond_to_missing?(method)
+    super unless CHILDREN.include?(method)
+  end
+
+  private
+
+  def parse_rows(garden_rows)
+    garden_rows.split("\n").map { |row| row.split("").map { |c| PLANTS[c] } }
+  end
+end
