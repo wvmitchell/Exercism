2657185d85f5babdfb54161a8b82b1ba28edee75
Will Mitchell <wvmitchell@gmail.com>
2024-09-05 10:29:03 -0400

    refactor

--- ruby/rotational-cipher/rotational_cipher.rb
+++ ruby/rotational-cipher/rotational_cipher.rb
@@ -1,27 +1,35 @@
 module RotationalCipher
   LETTERS = ("a".."z").to_a.freeze
-  UPCASE_LETTERS = ("A".."Z").to_a.freeze
 
   # rotate performs a rotational cipher
   # @param string [String] the string to rotate
   # @param rot [Integer] the number characters to rotate
+  # @return [String] the rotated string
   def self.rotate(string, rot)
     string
       .chars
       .map do |char|
         next char unless char =~ /[a-zA-Z]/
 
-        if UPCASE_LETTERS.index(char)
-          rotate_char(UPCASE_LETTERS, char, rot)
-        else
-          rotate_char(LETTERS, char, rot)
-        end
+        new_char = rotate_char(char, rot)
+        upcase?(char) ? new_char.upcase : new_char
       end
       .join
   end
 
-  def self.rotate_char(letters, char, rot)
-    rot_index = (letters.index(char) + rot) % 26
-    letters[rot_index]
+  # rotate_char rotates a single character
+  # @param char [String] the character to rotate
+  # @param rot [Integer] the number characters to rotate
+  # @return [String] the rotated character
+  def self.rotate_char(char, rot)
+    rot_index = (LETTERS.index(char.downcase) + rot) % 26
+    LETTERS[rot_index]
+  end
+
+  # upcase? checks if a character is uppercase
+  # @param char [String] the character to check
+  # @return [Boolean] true if the character is uppercase
+  def self.upcase?(char)
+    char.upcase == char
   end
 end
