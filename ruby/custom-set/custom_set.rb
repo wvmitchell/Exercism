869894f8cd3e2f617c20de66b7142c7c252c736c
Will Mitchell <wvmitchell@gmail.com>
2024-09-09 11:46:03 -0400

    solution to custom set in ruby

---
+++ ruby/custom-set/custom_set.rb
@@ -0,0 +1,28 @@
+class CustomSet
+  attr_reader :set
+
+  def initialize(elements)
+    @set = Set.new(elements)
+  end
+
+  def empty? = set.empty?
+
+  def member?(element) = set.member? element
+
+  def subset?(other) = set.subset? other.set
+
+  def disjoint?(other) = set.disjoint? other.set
+
+  def ==(other) = set.eql? other.set
+
+  def add(value)
+    set.add value
+    self
+  end
+
+  def intersection(other) = CustomSet.new(set.intersection(other.set))
+
+  def difference(other) = CustomSet.new(set.difference(other.set))
+
+  def union(other) = CustomSet.new(set.union(other.set))
+end
